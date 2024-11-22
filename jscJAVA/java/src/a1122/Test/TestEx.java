b35441cd069c99fed81d700e07104a5b37ff4acf
bababa <imf345666@gmail.com>
2024-11-22 17:32:52 +0900

    24.11.22

---
+++ jscJAVA/java/src/a1122/Test/TestEx.java
@@ -0,0 +1,11 @@
+package a1122.Test;
+
+public class TestEx {
+    public static void main(String[] args) {
+        
+        Student student = new Student("홍길동", 20231234, 90,85,92);
+
+
+        
+    }
+}
